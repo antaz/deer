ad1906fe7963dddae0ae4c73e591a5a019caf05e
nxf7 <>
2022-06-01 13:46:19 +0100

    add requirements and entrypoint to setup.cfg

--- setup.py
+++ setup.py
@@ -1,10 +1,2 @@
 from setuptools import setup
-
-# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="deer",
-    install_requires=[
-    ],
-    extras_require={
-    },
-)
+setup()
